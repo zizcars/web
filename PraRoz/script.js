cf43676d6168c539cd02fd41fa3a178b3f997833
Abdelaziz Chakkaf <abdelazizchakkaf@Abdelazizs-MacBook-Pro.local>
2025-05-20 09:20:33 +0100

    validite user and password

--- PraRoz/script.js
+++ PraRoz/script.js
@@ -1,21 +1,27 @@
 const user = document.getElementById("user");
-// const userValue = user.value;
+const password = document.getElementById("password");
 
-function validition(user) {
-	return /^[a-zA-Z][a-zA-Z0-9_]{3,16}/.test(user.value);
+function validitionUser(user) {
+	return /^[a-zA-Z_][a-zA-Z0-9_]{3,16}$/.test(user.value);
 }
-
-console.log(validition(user));
-
-if (validition(user)) {
-	console.log("logging seccess!!!");
-}
-else {
-	console.log("error!");
+function validitionPassword(password) {
+	return /^(?=.*\d)(?=.*[a-zA-Z]).{8,}$/.test(password.value);
 }
 
 document.addEventListener("submit", (e) => {
 	e.preventDefault();
-
+	if (validitionUser(user)) {
+		console.log("logging seccess!!!");
+	}
+	else {
+		console.log("unvalid username!");
+	}
+	if (validitionPassword(password)) {
+		console.log("logging seccess!!!");
+	}
+	else {
+		console.log("unvalid password!");
+	}
+	console.log(password.value);
 }
 );
