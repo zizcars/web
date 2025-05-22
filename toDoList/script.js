936f69614e245c89f50163faab29d456d5a62955
Abdelaziz Chakkaf <abdelazizchakkaf@Abdelazizs-MacBook-Pro.local>
2025-05-22 11:54:43 +0100

    add new tasks in the to do list

--- toDoList/script.js
+++ toDoList/script.js
@@ -1,4 +1,27 @@
-const addTaskButton = document.getElementById("add-task");
+const plusBtn = document.getElementById("plus-btn");
+const task = document.getElementById("task");
+const board = document.getElementById("board");
 
-button.addEventListener('click', (e)=>{
+function addNewTask(task, board){
+	if(task.value.trim().length !== 0){
+		const newTask = document.createElement('div');
+		newTask.className = "added-task";
+		
+		const newTaskCheckbox = document.createElement("input");
+		newTaskCheckbox.type = "checkbox";
+		newTaskCheckbox.className = "task-checkbox";
+		newTask.appendChild(newTaskCheckbox);
+	
+		const newTaskContent = document.createElement("div");
+		newTaskContent.className = "added-task-content";
+		newTaskContent.innerText = task.value;
+		newTask.appendChild(newTaskContent);
+	
+		board.appendChild(newTask);
+		task.value = "";
+	}
+}
+
+plusBtn.addEventListener('click', (e) => {
+	addNewTask(task, board);
 });
