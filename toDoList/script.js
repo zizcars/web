b1e3476dd6c57f30d8b90ae8cd6057b264b914ec
Abdelaziz Chakkaf <achakkaf@c2r3p7.1337.ma>
2025-05-22 20:30:04 +0100

    to-do list is done

--- toDoList/script.js
+++ toDoList/script.js
@@ -1,27 +1,49 @@
-const plusBtn = document.getElementById("plus-btn");
-const task = document.getElementById("task");
-const board = document.getElementById("board");
+function addNewTask() {
+	const task = document.getElementById("task");
+	const tasks = document.getElementById("tasks");
 
-function addNewTask(task, board){
-	if(task.value.trim().length !== 0){
+	if (task.value.trim().length !== 0) {
 		const newTask = document.createElement('div');
 		newTask.className = "added-task";
-		
+		newTask.id = "added-task";
+
 		const newTaskCheckbox = document.createElement("input");
 		newTaskCheckbox.type = "checkbox";
 		newTaskCheckbox.className = "task-checkbox";
+		newTaskCheckbox.id = "task-checkbox";
 		newTask.appendChild(newTaskCheckbox);
-	
+
 		const newTaskContent = document.createElement("div");
 		newTaskContent.className = "added-task-content";
+		newTaskContent.id = "added-task-content";
 		newTaskContent.innerText = task.value;
 		newTask.appendChild(newTaskContent);
-	
-		board.appendChild(newTask);
+
+		const newCloseBtn = document.createElement("button");
+		newCloseBtn.className = "close-btn";
+		newCloseBtn.id = "close-btn";
+		newTask.appendChild(newCloseBtn);
+
+		tasks.appendChild(newTask);
 		task.value = "";
 	}
 }
 
-plusBtn.addEventListener('click', (e) => {
-	addNewTask(task, board);
+document.getElementById("plus-btn").addEventListener('click', (e) => {
+	addNewTask();
 });
+
+document.getElementById("task").addEventListener("keypress", (e) => {
+	if (e.key == "Enter") {
+		addNewTask();
+	}
+});
+
+document.getElementById('board').addEventListener('click', (e) => {
+	if (e.target.classList.contains('close-btn')) {
+		e.target.closest('.added-task').remove();
+	}
+});
+
+// const checkbox = document.querySelector(".tasks");
+// console.log(checkbox.childElementCount);
